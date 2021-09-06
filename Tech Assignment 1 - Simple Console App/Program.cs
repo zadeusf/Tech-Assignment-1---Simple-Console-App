8f03f5b643b62d07f52f42edd41cf81c0af27b81
Zade <Zadepata@gmail.com>
2021-09-06 15:17:36 -0400

    Add project files.

---
+++ Tech Assignment 1 - Simple Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_1___Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Void..");
+        }
+    }
+}
